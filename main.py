a87e0d98857e0822ad1d51e35e5fac211dfc2e9a
Denis Serebriakov <denbartolomee@gmail.com>
2026-05-15 11:33:44 +0300

    fix interactive test flow

--- main.py
+++ main.py
@@ -166,8 +166,14 @@ async def handle_open_interactive_test(
         await facade.answer_text(text=texts.interactive_test_not_found)
         return
 
+    media = [
+        types.PhotoAttachmentRequest(payload=types.PhotoAttachmentRequestPayload(token=interactive_test.photo)),
+    ]
+
     keyboard = keyboards.interactive_test_menu(interactive_test=interactive_test)
-    await facade.edit_message(text=interactive_test.description, keyboard=keyboard)
+
+    await facade.delete_message()
+    await facade.answer(text=interactive_test.title, media=media, keyboard=keyboard)
 
 
 @router.message_callback(callbacks.ProceedInteractiveTest.filter())
@@ -186,17 +192,21 @@ async def handle_proceed_interactive_test(
     for result in interactive_test.results:
         await state.update_data({result.slug: 0})
 
-    question_text = interactive_test.questions[0].question
+    question = interactive_test.questions[0]
+
+    media = [
+        types.PhotoAttachmentRequest(payload=types.PhotoAttachmentRequestPayload(token=question.photo)),
+    ]
 
     text = texts.interactive_test_question_wrapper.format(
         test_title=interactive_test.title,
         question_number=1,
         total_questions=len(interactive_test.questions),
-        question=question_text,
+        question=question.question,
     )
     keyboard = keyboards.proceed_interactive_test_menu(interactive_test=interactive_test, question_index=0)
 
-    await facade.edit_message(text=text, keyboard=keyboard)
+    await facade.edit_message(text=text, media=media, keyboard=keyboard)
 
 
 @router.message_callback(callbacks.InteractiveTestOption.filter())
@@ -231,6 +241,9 @@ async def handle_interactive_test_question_answer(
         await state.clear()
 
         await facade.edit_message(text=best_result.description)
+        await facade.send_message(text=texts.main_menu, keyboard=keyboards.main_menu)
+
+        return
 
     question_index += 1
     next_question = interactive_test.questions[question_index]
@@ -243,9 +256,12 @@ async def handle_interactive_test_question_answer(
     )
     keyboard = keyboards.proceed_interactive_test_menu(interactive_test=interactive_test, question_index=question_index)
 
-    await facade.edit_message(text=text, keyboard=keyboard)
+    media = [
+        types.PhotoAttachmentRequest(payload=types.PhotoAttachmentRequestPayload(token=next_question.photo)),
+    ]
 
-    await facade.send_message(text=texts.main_menu, keyboard=keyboards.main_menu)
+    await facade.edit_message(text=text, media=media, keyboard=keyboard)
+
 
 @router.message_callback(callbacks.MiniTestsList.filter())
 async def handle_mini_tests_list(
